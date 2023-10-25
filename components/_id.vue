9e5d2759a02a614af40e44b23fa9e54bd8feb765
Pyperdok <zhitov.egoriko@mail.ru>
2023-10-25 05:31:21 +0300

    commit

---
+++ components/_id.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>posts</h1>
+        <p>1</p>
+        <nuxt-link to="/posts">Back to posts list</nuxt-link>
+    </div>
+</template>
+
+<script>
+export default {
+//   async asyncData({ params, $axios }) {
+//     // Fetch the post data based on its ID.
+//     // Assuming you have an API endpoint like /api/posts/{id}
+//     // If you're not using Axios, replace this with your method of fetching data.
+    
+//     const post = await $axios.$get(`/api/posts/${params.id}`);
+    
+//     return { post };
+//   }
+}
+</script>
